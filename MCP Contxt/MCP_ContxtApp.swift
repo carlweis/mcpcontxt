9a72a015ab3ee6295b00c3995292ed0091ea8b58
Carl Weis <carl@carlweis.com>
2026-01-31 16:54:27 -0500

    Initial Commit

---
+++ MCP Contxt/MCP_ContxtApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCP_ContxtApp.swift
+//  MCP Contxt
+//
+//  Created by Carl on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct MCP_ContxtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
